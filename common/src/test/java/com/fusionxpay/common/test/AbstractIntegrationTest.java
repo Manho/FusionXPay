eae9ffc2fa806dbb0ab7acbf5b79f1fba13bb078
Wenhao <manhodl@gmail.com>
2026-02-08 17:52:43 +0800

    fix: enforce mysql dialect for shared integration tests

--- common/src/test/java/com/fusionxpay/common/test/AbstractIntegrationTest.java
+++ common/src/test/java/com/fusionxpay/common/test/AbstractIntegrationTest.java
@@ -35,6 +35,8 @@ public abstract class AbstractIntegrationTest {
         registry.add("spring.datasource.username", mysql::getUsername);
         registry.add("spring.datasource.password", mysql::getPassword);
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQLDialect");
+        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.MySQLDialect");
 
         // Redis
         registry.add("spring.data.redis.host", redis::getHost);
