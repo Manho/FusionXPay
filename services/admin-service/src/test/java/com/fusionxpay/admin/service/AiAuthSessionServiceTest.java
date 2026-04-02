c4e13bf05908f3293f9b9effd4b7e49eeba9666e
Wenhao <manhodl@gmail.com>
2026-04-02 21:18:18 +0800

    test: isolate ai auth session datasource config

--- services/admin-service/src/test/java/com/fusionxpay/admin/service/AiAuthSessionServiceTest.java
+++ services/admin-service/src/test/java/com/fusionxpay/admin/service/AiAuthSessionServiceTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.Instant;
 
@@ -26,6 +27,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest(classes = AdminApplication.class)
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "eureka.client.enabled=false",
+        "spring.cloud.discovery.enabled=false"
+})
 class AiAuthSessionServiceTest {
 
     @Autowired
